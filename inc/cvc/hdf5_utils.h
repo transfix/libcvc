187ebad60dbe8824348ed1f3172c4b333dab797c
Joe Rivera <j@jriv.us>
2026-04-21 20:23:59 -0500

    hdf5_utils: qualify boost::make_tuple to fix ADL clash with std::make_tuple

--- inc/cvc/hdf5_utils.h
+++ inc/cvc/hdf5_utils.h
@@ -956,7 +956,7 @@ namespace CVC_NAMESPACE
                                    % error.getDetailMsg()));
         }
 
-      return make_tuple(data,actualDim);
+      return boost::make_tuple(data,actualDim);
     }
 
     // ---------------------
